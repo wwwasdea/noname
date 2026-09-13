64391f9b871c9a311046612e3a828df625a21d3a
qingfeng <3205486470@qq.com>
2026-09-13 10:28:21 +0800

    孙皓报错

--- apps/core/character/sp/skill.js
+++ apps/core/character/sp/skill.js
@@ -40776,18 +40776,15 @@ const skills = {
 		},
 		autodelay: true,
 		async content(event, trigger, player) {
-			await player
-				.chooseToDiscard(true, "he", card => {
-					const { player, usefulCards } = get.event();
-					if (usefulCards.includes(card)) {
-						return 0.1;
-					}
+			await player.chooseToDiscard({
+				forced: true,
+				position: "he",
+				ai(card) {
+					const player = get.player();
+					if (player.hasUseTarget(card)) return 0.1;
 					return 20 - get.value(card);
-				})
-				.set(
-					"usefulCards",
-					player.getDiscardableCards(player, "h", card => player.getUseValue(card))
-				);
+				},
+			});
 		},
 	},
 	rechouhai: {
